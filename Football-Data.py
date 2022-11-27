99fa6309b7b8a973f3d95653ed4f5954797d4053
pedro castro <pedro.castr8@gmail.com>
2022-11-27 12:51:14 -0300

    initial commit

---
+++ Football-Data.py
@@ -0,0 +1,19 @@
+# Packages
+import pandas as pd
+import numpy as np
+import seaborn
+import json
+
+# jogos_hoje = pd.read_csv()
+
+nome = 'gerrad'
+camisa = 8
+
+#Condicionais
+
+if camisa == 10:
+    print("jogo de futibas do", nome, 'que veste a camisa',camisa)
+elif nome == 'gerrard':
+    print("jogo de futibas do", nome, 'que veste a camisa',camisa)
+else:
+    print("jogo de futibas do", nome, 'que veste a camisa',camisa)
